b76d2715591e560adab139029918147b0aaeaed3
techbash.exe <emretekbas97@gmail.com>
2022-12-04 00:04:54 +0000

    secret key exposed

--- techbash/techbash/settings.py
+++ techbash/techbash/settings.py
@@ -19,6 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
+
+
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
